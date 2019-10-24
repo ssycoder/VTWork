3f12b75996cea9983cbd4e9c40454b78774451da
ssycoder <56398107+ssycoder@users.noreply.github.com>
2019-10-24 08:06:56 -0500

    Add files via upload

---
+++ SelectRecords.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+//STEP 1. Import required packages
+import java.io.IOException;
+import java.sql.*;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class SelectRecords {
+   // JDBC driver name and database URL
+   static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";  
+   static final String DB_URL = "jdbc:mysql://localhost/STUDENTS";
+
+   //  Database credentials
+   static final String USER = "root";
+   static final String PASS = "";
+   
+   public static final int FILE_SIZE = 1024;
+   
+   public static void main(String[] args) {
+   Connection conn = null;
+   Statement stmt = null;
+   try{
+      //STEP 2: Register JDBC driver
+      Class.forName("com.mysql.cj.jdbc.Driver");
+
+      //STEP 3: Open a connection
+      System.out.println("Connecting to a selected database...");
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+      System.out.println("Connected database successfully...");
+      
+      //STEP 4: Execute a query
+      System.out.println("Creating statement...");
+      stmt = conn.createStatement();
+
+      String sql = "SELECT id, firstName, lastName, age FROM Registration";
+      ResultSet rs = stmt.executeQuery(sql);
+      //STEP 5: Extract data from result set
+      while(rs.next()){
+         //Retrieve by column name
+         int id  = rs.getInt("id");
+         int age = rs.getInt("age");
+         String first = rs.getString("firstName");
+         String last = rs.getString("lastName");
+
+         //Display values
+         System.out.print("ID: " + id);
+         System.out.print(", Age: " + age);
+         System.out.print(", First: " + first);
+         System.out.println(", Last: " + last);
+      }
+      rs.close();
+   }catch(SQLException se){
+      //Handle errors for JDBC
+      se.printStackTrace();
+   }catch(Exception e){
+      //Handle errors for Class.forName
+      e.printStackTrace();
+   }finally{
+      //finally block used to close resources
+      try{
+         if(stmt!=null)
+            conn.close();
+      }catch(SQLException se){
+      }// do nothing
+      try{
+         if(conn!=null)
+            conn.close();
+      }catch(SQLException se){
+         se.printStackTrace();
+      }//end finally try
+   }//end try
+   //***********************
+   
+    
+  
+    
+ 
+  Logger logger = Logger.getLogger(SelectRecords.class.getName());
+  
+ 
+  try {
+ 
+ 
+// Create an instance of FileHandler with 5 logging files sequences.
+ 
+ 
+FileHandler handler = new FileHandler("sample.log", FILE_SIZE, 5, true);
+ 
+ 
+handler.setFormatter(new SimpleFormatter());
+ 
+ 
+logger.addHandler(handler);
+ 
+ 
+logger.setUseParentHandlers(false);
+ 
+  } catch (IOException e) {
+ 
+ 
+logger.warning("Failed to initialize logger handler.");
+ 
+  }
+  
+ 
+  logger.info("Logging info message.");
+ 
+  logger.warning("Logging warn message.");
+  
+  logger.info("Data sucesssfully written to data.");
+  
+ 
+    
+   
+   //**********************
+   System.out.println("Goodbye!");
+}//end main
+}//end JDBCExample
